05ce8c0bdb0a764649a5887c041a5ad6f3bafabe
Johnny Choudhury-Lucas <client-werewolf-insulin@jch.lu>
2018-06-14 16:05:07 +0100

    Initial commit

---
+++ tests/BasicTestCase.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace MinimumViableTests\Tests;
+
+use \PHPUnit\Framework\TestCase;
+
+class BasicTestCase extends TestCase
+{
+    public function testCase()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testAnotherCase()
+    {
+        $this->assertFalse(false);
+    }
+}
